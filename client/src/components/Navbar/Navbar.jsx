c9c7f31534b154533c7265eb5379091b056cd182
letnagao <leticia-nagao@hotmail.com>
2023-04-08 19:03:40 -0300

    Added Navbar design

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -4,6 +4,8 @@ import SearchIcon from "@mui/icons-material/Search";
 import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
 import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
 import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import { Link } from "react-router-dom";
+import "./Navbar.scss";
 
 const Navbar = () => {
   return (
@@ -18,9 +20,44 @@ const Navbar = () => {
             <span>USD</span>
             <KeyboardArrowDownIcon />
           </div>
+          <div className="item">
+            <Link className="link" to="/products/1">Women</Link>
+          </div>
+          <div className="item">
+            <Link className="link" to="/products/2">Men</Link>
+          </div>
+          <div className="item">
+            <Link className="link" to="/products/3">Children</Link>
+          </div>
         </div>
-        <div className="center"></div>
-        <div className="right"></div>
+
+        <div className="center">
+          <Link className="link" to="/">POLARIS</Link>
+        </div>
+
+        <div className="right">
+          <div className="item">
+            <Link className="link" to="/">Homepage</Link>
+          </div>
+          <div className="item">
+            <Link className="link" to="/">About</Link>
+          </div>
+          <div className="item">
+            <Link className="link" to="/">Contact</Link>
+          </div>
+          <div className="item">
+            <Link className="link" to="/">Stores</Link>
+          </div>
+          <div className="icons">
+            <SearchIcon />
+            <PersonOutlineOutlinedIcon />
+            <FavoriteBorderOutlinedIcon />
+            <div className="cardIcon">
+              <ShoppingCartOutlinedIcon />
+              <span>0</span>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
